534b0bfa0314c3ae552c3badf8feccf32978d4ae
sugurun4946 <sub.j.mi3000@gmail.com>
2026-06-17 00:54:48 +0900

    Day2

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 //家計簿の構造体定義
 struct Expense {
@@ -11,34 +12,24 @@ struct Expense {
 
 int main()
 {
-    //オブジェクト生成
-    Expense expense;
+  std::vector<Expense> expenses; //vector型の定義(Expense構造体を格納する可変長配列、expensesの宣言)
 
-   //変数定義
-    int amount;
-    std::string item;
-    std::string category; 
-    std::string memo;
+  for(int i = 0; i < 2; i++)
+  {
+    Expense expense; //オブジェクト生成
 
-    //int amount; //金額
-    //std::string category; //出費のカテゴリ
-
+    std::cout << "\n" << i + 1 << "件目\n";
     std::cout<<"金額を入力してください"<<std::endl;
-    std::cin>>amount;
+    std::cin>>expense.amount;
 
     std::cout<<"品目を入力してください"<<std::endl;
-    std::cin>>item;
+    std::cin>>expense.item;
 
     std::cout<<"カテゴリを入力してください"<<std::endl;
-    std::cin>>category;
+    std::cin>>  expense.category;
 
     std::cout<<"メモを入力してください"<<std::endl;
-    std::cin>>memo;
-
-    expense.amount = amount;
-    expense.item = item;
-    expense.category = category;
-    expense.memo = memo;
+    std::cin>>expense.memo;
 
     std::cout << "\n----- 登録内容 -----\n";
     std::cout << "金額: " << expense.amount << "円\n";
@@ -46,6 +37,28 @@ int main()
     std::cout << "品目: " << expense.item << "\n";
     std::cout << "メモ: " << expense.memo << "\n";
 
+    expenses.push_back(expense); //expenses配列に入力内容を追加する
+
+  }
+
+  std::cout << "\n===== 一覧 =====\n";
+  
+  //入力一覧の表示
+  for (const auto& expense : expenses)
+  {
+    std::cout
+        << expense.amount << "円 "
+        << expense.item << " "
+        << expense.category << " "
+        << expense.memo << "\n";
+  }
+  //合計金額の算出
+  int total = 0;
+  for (const auto& expense : expenses) 
+  {
+    total += expense.amount;
+  }
+    std::cout << "合計金額: " << total << "円\n";
 
   return 0;
 
